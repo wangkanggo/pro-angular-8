53a72aa9aae9b0687d02b756502ebec8114ea50a
wkangwk <wkangwk@cn.ibm.com>
2019-08-30 15:30:53 +0800

    copied from 6

--- 07-SportsStore/SportsStore/src/app/app.component.ts
+++ 07-SportsStore/SportsStore/src/app/app.component.ts
@@ -1,10 +1,7 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: "app",
+    template: "<store></store>"
 })
-export class AppComponent {
-  title = 'SportsStore';
-}
+export class AppComponent { }
--- 07-SportsStore/SportsStore/src/app/app.module.ts
+++ 07-SportsStore/SportsStore/src/app/app.module.ts
@@ -1,16 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { StoreModule } from "./store/store.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [BrowserModule, StoreModule],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
